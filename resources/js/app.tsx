bdb135e77e2818af3a0a89435dc475bab2151211
ecreeth <luismiguel1730@gmail.com>
2020-05-14 19:39:08 -0400

    wip

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,35 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
 
-import { Header, Aside } from './components';
-import MainContent from './components/MainContent';
+import { Header, Aside, MainContent } from './components/index';
 
 const app: any = document.getElementById('app');
 
 function App() {
   return (
-    <div className="font-sans m-0 antialiased leading-none bg-gray-600">
-      <Header />
-      <section className="container mx-auto">
-        <div className="flex w-full min-w-full rounded-sm mt-4">
-          <Aside />
-          <main className="p-4 w-full bg-gray-800 ml-4 font-light mb-5 rounded shadow-lg">
-            <MainContent
-              initialPage={JSON.parse(app.dataset.page)}
-              resolveComponent={name =>
-                React.lazy(() => import(`./pages/${name}`))
-              }
-            />
-          </main>
-        </div>
-      </section>
-    </div>
+    <RecoilRoot initializeState={() => {}}>
+      <div className="font-sans m-0 antialiased leading-none bg-gray-600">
+        <Header />
+        <section className="container mx-auto">
+          <div className="flex w-full min-w-full rounded-sm mt-4">
+            <Aside />
+            <main className="p-4 w-full bg-gray-800 ml-4 font-light mb-5 rounded shadow-lg">
+              <MainContent
+                initialPage={JSON.parse(app.dataset.page)}
+                resolveComponent={name =>
+                  React.lazy(() => import(`./pages/${name}`))
+                }
+              />
+            </main>
+          </div>
+        </section>
+      </div>
+    </RecoilRoot>
   );
 }
 
-ReactDOM.render(
-  <RecoilRoot initializeState={() => {}}>
-    <App />
-  </RecoilRoot>,
-  app
-);
+ReactDOM.render(<App />, app);
--- resources/js/components/index.ts
+++ resources/js/components/index.ts
@@ -2,3 +2,4 @@ export { default as Aside } from './Aside';
 export { default as Header } from './Header';
 export { default as Spinner } from './Spinner';
 export { default as Link } from './Link';
+export { default as MainContent } from './MainContent';
